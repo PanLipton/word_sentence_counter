83faafa183312ebf47e1581fa9ada989cf85d0dd
PanLipton <panskillsasda@gmail.com>
2024-04-03 09:27:37 +0300

    Create test for word count function

--- test_word_sentence_counter.py
+++ test_word_sentence_counter.py
@@ -4,3 +4,6 @@ from word_sentence_counter import word_count, sentence_count, count_words_senten
 @pytest.fixture
 def example_text():
     return "This is a sample text. It contains 3 sentences! Can you count them?"
+
+def test_word_count(example_text):
+    assert word_count(example_text) == 14
