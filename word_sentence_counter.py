6003604546e1b60491ddf5727094811cf24960d1
PanLipton <panskillsasda@gmail.com>
2024-04-03 09:12:26 +0300

    Create sentence count function

--- word_sentence_counter.py
+++ word_sentence_counter.py
@@ -4,3 +4,7 @@ def word_count(text):
     words = re.findall(r'\b\w+\b', text)
     return len(words)
 
+def sentence_count(text):
+    sentences = re.split(r'[.!?]+', text)
+    return len([s for s in sentences if s.strip()])
+
