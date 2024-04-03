b3c2ffdfd8e25b4222b79e88e528cdc52305b2be
PanLipton <panskillsasda@gmail.com>
2024-04-03 09:16:32 +0300

    Create words count and sentences function

--- word_sentence_counter.py
+++ word_sentence_counter.py
@@ -8,3 +8,9 @@ def sentence_count(text):
     sentences = re.split(r'[.!?]+', text)
     return len([s for s in sentences if s.strip()])
 
+def count_words_sentences(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+        words = word_count(text)
+        sentences = sentence_count(text)
+        return words, sentences
